c468af8a036cb7fbc62c353b71c58127d19dcbe7
Sveta-ls <slana6570@gmail.com>
2025-12-26 18:12:22 +0300

    Updates

--- _next/static/WIp0bLBq1cKsD-8b_PbGm/_buildManifest.js
+++ _next/static/WIp0bLBq1cKsD-8b_PbGm/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/xmas_gift//_next/:path+",
-        "destination": "/xmas_gift/_next/:path+"
+        "source": "/xmas//_next/:path+",
+        "destination": "/xmas/_next/:path+"
       }
     ],
     "fallback": []
